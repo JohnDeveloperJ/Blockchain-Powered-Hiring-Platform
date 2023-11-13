a7a024106d392a4682e7c0378e7fa03dfcb3263f
john j <johnjjdev690@gmail.com>
2023-11-13 16:40:34 +0000

    git add .

--- contracts/FairHiring.sol
+++ contracts/FairHiring.sol
@@ -1,34 +1,100 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+/// @title A contract for fair hiring practices and standardized application scoring
+/// @dev Inherits from OpenZeppelin's Ownable contract for ownership management
+contract FairHiring is Ownable(msg.sender) {
 
-    event Withdrawal(uint amount, uint when);
+    /// @notice Event emitted when a job is added
+    /// @param jobId The ID of the job added
+    /// @param title The title of the job
+    event JobAdded(uint256 jobId, string title);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    /// @notice Event emitted when a candidate's application is scored
+    /// @param candidateId The ID of the candidate
+    /// @param totalScore The total score of the candidate's application
+    event CandidateScored(uint256 candidateId, uint256 totalScore);
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    struct Job {
+        uint256 id;
+        string title;
+        string description;
+        bool isActive;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    struct Candidate {
+        uint256 id;
+        uint256 jobId;
+        uint256 experienceScore;
+        uint256 educationScore;
+        uint256 skillScore;
+        uint256 totalScore;
+        bool scored;
     }
+
+    Job[] public jobs;
+    Candidate[] public candidates;
+    mapping(uint256 => uint256[]) private jobToCandidates;
+
+    /// @notice Adds a new job to the contract
+    /// @param _title The title of the job
+    /// @param _description The description of the job
+    /// @dev Only callable by the owner (HR manager)
+    function addJob(string calldata _title, string calldata _description) external onlyOwner {
+        uint256 jobId = jobs.length;
+        jobs.push(Job({
+            id: jobId,
+            title: _title,
+            description: _description,
+            isActive: true
+        }));
+        emit JobAdded(jobId, _title);
+    }
+
+    /// @notice Scores a candidate's application
+    /// @param _candidateId The ID of the candidate
+    /// @param _experiencePoints Points awarded for experience
+    /// @param _educationPoints Points awarded for education
+    /// @param _skillPoints Points awarded for skills
+    /// @dev Only callable by the owner (HR manager)
+    /// @dev Emits the CandidateScored event
+    function scoreCandidate(
+        uint256 _candidateId,
+        uint256 _experiencePoints,
+        uint256 _educationPoints,
+        uint256 _skillPoints
+    ) external onlyOwner {
+        require(_candidateId < candidates.length, "Candidate does not exist.");
+        require(!candidates[_candidateId].scored, "Candidate has already been scored.");
+
+        Candidate storage candidate = candidates[_candidateId];
+        candidate.experienceScore = _experiencePoints;
+        candidate.educationScore = _educationPoints;
+        candidate.skillScore = _skillPoints;
+        candidate.totalScore = calculateTotalScore(_experiencePoints, _educationPoints, _skillPoints);
+        candidate.scored = true;
+
+        emit CandidateScored(_candidateId, candidate.totalScore);
+    }
+
+    /// @notice Calculates the total score for a candidate
+    /// @param _experiencePoints Points awarded for experience
+    /// @param _educationPoints Points awarded for education
+    /// @param _skillPoints Points awarded for skills
+    /// @return The total score for the candidate
+    function calculateTotalScore(
+        uint256 _experiencePoints,
+        uint256 _educationPoints,
+        uint256 _skillPoints
+    ) private pure returns (uint256) {
+        return _experiencePoints + _educationPoints + _skillPoints; // Simplified scoring
+    }
+
+    // ... other functions and state variables
+
+    // Note: The implementation of the actual application process, review process, and other features
+    // have been omitted for brevity. They would be implemented similarly, with proper access control
+    // and event emissions.
 }
