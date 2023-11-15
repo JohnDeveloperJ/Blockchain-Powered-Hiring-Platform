ca36ae1a0287c084927236f0d4a4ca57e0d5df36
john j <johnjjdev690@gmail.com>
2023-11-15 17:56:27 +0000

    git add .

--- fair-hiring-frontend/src/AddJob.js
+++ fair-hiring-frontend/src/AddJob.js
@@ -1,32 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import contractABI from './path_to_ABI.json'; // ABI of your contract
 
-function AddJob({ contract }) {
-  const [jobTitle, setJobTitle] = useState('');
-  const [jobDescription, setJobDescription] = useState('');
+const contractAddress = "YOUR_CONTRACT_ADDRESS";
+
+function App() {
+  const [provider, setProvider] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  const [newJobTitle, setNewJobTitle] = useState('');
+  const [newJobDescription, setNewJobDescription] = useState('');
+
+  useEffect(() => {
+    const init = async () => {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const contract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
+        setProvider(provider);
+        setContract(contract);
+        fetchJobs();
+      } else {
+        console.error("Ethereum object not found, install MetaMask.");
+      }
+    };
+
+    init();
+  }, []);
+
+  const fetchJobs = async () => {
+    if (contract) {
+      const jobCount = await contract.jobs.length;
+      let jobList = [];
+      for (let i = 0; i < jobCount; i++) {
+        let job = await contract.jobs(i);
+        jobList.push(job);
+      }
+      setJobs(jobList);
+    }
+  };
 
   const handleAddJob = async () => {
-    // Call the addJob function from the smart contract
-    const tx = await contract.addJob(jobTitle, jobDescription);
-    await tx.wait();
-    alert('Job added!');
+    if (contract) {
+      await contract.addJob(newJobTitle, newJobDescription);
+      fetchJobs(); // Refresh the job list
+    }
   };
 
   return (
-    <div>
-      <input
-        value={jobTitle}
-        onChange={(e) => setJobTitle(e.target.value)}
-        placeholder="Job Title"
-      />
-      <input
-        value={jobDescription}
-        onChange={(e) => setJobDescription(e.target.value)}
-        placeholder="Job Description"
-      />
-      <button onClick={handleAddJob}>Add Job</button>
+    <div className="App">
+      <h1>Fair Hiring Interface</h1>
+      <div>
+        <input 
+          type="text" 
+          placeholder="Job Title" 
+          value={newJobTitle} 
+          onChange={(e) => setNewJobTitle(e.target.value)} 
+        />
+        <textarea 
+          placeholder="Job Description" 
+          value={newJobDescription} 
+          onChange={(e) => setNewJobDescription(e.target.value)} 
+        />
+        <button onClick={handleAddJob}>Add Job</button>
+      </div>
+      <div>
+        <h2>Job List</h2>
+        {jobs.map((job, index) => (
+          <div key={index}>
+            <h3>{job.title}</h3>
+            <p>{job.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default AddJob;
+export default App;
